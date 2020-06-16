8305530e6184de0a821165061e43f8d71477ab97
Albert Chen <chena@broadinstitute.org>
2020-06-16 05:37:14 -0400

    update server to NGINX

--- server/run_server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Instructions from https://stackoverflow.com/questions/16420092/how-to-make-python-script-run-as-service
-
-systemctl daemon-reload && systemctl enable covid && systemctl start covid --no-block
-
-# To view logs:
-systemctl status covid
-
