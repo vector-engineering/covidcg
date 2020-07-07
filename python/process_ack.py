0bac941fb05acb6acdb091f17dcc2b203a66b9c8
Albert Chen <chena@broadinstitute.org>
2020-07-07 18:04:32 -0400

    Try to shorten author names, by pulling last name and adding 'et al'

--- python/process_ack.py
+++ python/process_ack.py
@@ -32,6 +32,11 @@ def process_ack():
         _df = pd.read_excel(f, skiprows=[0, 1, 3])
         ack_df = pd.concat([ack_df, _df], ignore_index=True)
 
+    # Strip strings
+    ack_df["Originating lab"] = ack_df["Originating lab"].str.strip()
+    ack_df["Submitting lab"] = ack_df["Submitting lab"].str.strip()
+    ack_df["Authors"] = ack_df["Authors"].str.strip()
+
     print("done")
 
     print("Factorizing acknowledgements into IDs...", end="", flush=True)
@@ -45,6 +50,85 @@ def process_ack():
     unique_ack_df = pd.DataFrame.from_records(
         uniques, columns=["Originating lab", "Submitting lab", "Authors"]
     )
+
+    # Create an abbreviated authors column - with (First author) Last name et al.
+
+    # Default to the most common format - First-Name Last-Name, ...
+    # Exclude commas/semicolons since we want to capture all characters, including weird utf-8 ones
+    # Also have to specify both the english and chinese comma characters
+    unique_ack_df["authors_abbrev"] = (
+        unique_ack_df["Authors"]
+        .str.extract(r"^(?:[^,，;\s]+\s)*([^,，;]+)[,，;]")
+        .squeeze()  # DataFrame of one column -> Series
+        .str.strip()
+    )
+    # If the format is Last-Name Initials, ...
+    # i.e., FirstName L-N, or FirstName L.N, or FirstName L.N., or FirstName LN
+    # Then extract the last name
+    last_name_initial_inds = (
+        unique_ack_df["Authors"]
+        .str.contains(r"^[^,，;]+[\s,，;][A-Z]{1}(?:[-\.]{0,2}[A-Z])?\.?[,，;]")
+        .fillna(False)
+    )
+    unique_ack_df.loc[last_name_initial_inds, "authors_abbrev"] = (
+        unique_ack_df.loc[last_name_initial_inds, "Authors"]
+        .str.extract(
+            # This is a mess. I can't believe it kind of works.
+            # TODO: Split this up into more specific cases
+            r"^([\w’\'\-]+\s?[\w’\'\-]*)[,;\s](?:[^,，;]+)?[A-Z]{1}[-\.]{0,2}[A-Z]?\.?[,，;]"
+        )
+        .squeeze()
+        .str.strip()
+    )
+
+    # If the format is already in "LastName et al", then keep the Authors
+    et_al_inds = unique_ack_df["Authors"].str.contains(r"etl? al\.?$").fillna(False)
+    unique_ack_df.loc[et_al_inds, "authors_abbrev"] = (
+        unique_ack_df.loc[et_al_inds, "Authors"]
+        .str.extract(r"^(.+)etl? al\.$")
+        .squeeze()
+        .str.strip()
+    )
+
+    # Capitalize name and add "et al."
+    unique_ack_df["authors_abbrev"] = (
+        unique_ack_df["authors_abbrev"].str.capitalize() + " et al."
+    )
+
+    # If the format is one name: "LastName, F."
+    one_name_first_initial_inds = (
+        unique_ack_df["Authors"]
+        .str.contains(r"^([\w]+),?(?:[^,，;]+)?[A-Za-z]{1}[-\.]{0,2}[A-Za-z]?\.?$")
+        .fillna(False)
+    )
+    # unique_ack_df.loc[one_name_first_initial_inds, "authors_abbrev"] = (
+    #     unique_ack_df.loc[one_name_first_initial_inds, "Authors"]
+    #     .str.extract(r"^([\w]+),?(?:[^,，;]+)?[A-Za-z]{1}[-\.]{0,2}[A-Za-z]?\.?$")
+    #     .squeeze()
+    #     .str.strip()
+    # )
+    unique_ack_df.loc[
+        one_name_first_initial_inds, "authors_abbrev"
+    ] = unique_ack_df.loc[one_name_first_initial_inds, "Authors"]
+
+    # If the format is one name: "FirstName LastName"
+    # I guess I can't know whether the first string is the first name or not
+    # without making assumptions based on the names themselves
+    # But I'll assume english naming convention here (extract the last part as last name)
+    one_name_first_last_inds = (
+        unique_ack_df["Authors"]
+        .str.contains(r"^(?:[^,，;]{2,})\s([^,，;]{2,})$")
+        .fillna(False)
+    ) & (~one_name_first_initial_inds)
+    # unique_ack_df.loc[one_name_first_last_inds, "authors_abbrev"] = (
+    #     unique_ack_df.loc[one_name_first_last_inds, "Authors"]
+    #     .str.extract(r"^(?:[^\.,，;]{2,})\s([^\.,，;]{2,})$")
+    #     .squeeze()
+    #     .str.strip()
+    # )
+    unique_ack_df.loc[one_name_first_last_inds, "authors_abbrev"] = unique_ack_df.loc[
+        one_name_first_last_inds, "Authors"
+    ]
 
     # Append code to acknowledgement dataframe, and take
     # subset of columns
