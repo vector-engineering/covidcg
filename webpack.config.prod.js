604263712339089843bc41b9c4af336f8b00f4f0
Albert Chen <chena@broadinstitute.org>
2020-07-18 01:17:38 -0400

    unset the public path in the webpack config so it uses the webpack-public-path.js file on runtime instead

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,7 +24,7 @@ export default {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '',
     filename: '[name].[contenthash].js',
   },
   plugins: [
