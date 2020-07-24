ed301dcc9fe8675c03839beeda96fa787f1bb3c1
Albert Chen <chena@broadinstitute.org>
2020-07-24 08:36:00 -0400

    fix typo

--- server/decrypt_function/test_script.sh
+++ server/decrypt_function/test_script.sh
@@ -9,7 +9,7 @@ functions-framework --target=decrypt_accession_id --debug
 # Body:
 # {
 # 	"accession_ids": [
-#       "62da3a1bdaec4495d689f7211287d70e2801cd5e899ea0afd84c0fdb20b92a8c", # 
+#       "62da3a1bdaec4495d689f7211287d70e2801cd5e899ea0afd84c0fdb20b92a8c",
 #       "bb1c5afb8a003a053f5668bfec64710e32ef7559e0d8d929da5856be8c9014ff",
 # 	    "asdf"
 #   ]
