797f3855099d5516a94264f337341b4b31389ceb
Albert Chen <chena@broadinstitute.org>
2020-08-14 18:53:58 -0400

    GISAID requested formatting

--- src/components/Pages/AboutTab.js
+++ src/components/Pages/AboutTab.js
@@ -44,117 +44,91 @@ const AboutTab = () => {
           <a id="contributors"></a>
           <span className="section-title">COVID-CG is developed by</span>
 
-          <div className="content-block">
-            <div className="content-text">
-              <p>
-                COVID CoV Genomics (CG) was developed in the{' '}
-                <ExternalLink href="https://vector-engineering.github.io/">
-                  Vector Engineering Lab
-                </ExternalLink>{' '}
-                (
-                <ExternalLink href="https://www.broadinstitute.org/stanley">
-                  Stanley Center for Psychiatric Research
-                </ExternalLink>
-                ,{' '}
-                <ExternalLink href="https://www.broadinstitute.org/">
-                  Broad Institute of MIT and Harvard
-                </ExternalLink>
-                ), by:
-              </p>
+          <div className="content-text">
+            <p>
+              COVID CoV Genomics (CG) was developed in the{' '}
+              <ExternalLink href="https://vector-engineering.github.io/">
+                Vector Engineering Lab
+              </ExternalLink>{' '}
+              (
+              <ExternalLink href="https://www.broadinstitute.org/stanley">
+                Stanley Center for Psychiatric Research
+              </ExternalLink>
+              ,{' '}
+              <ExternalLink href="https://www.broadinstitute.org/">
+                Broad Institute of MIT and Harvard
+              </ExternalLink>
+              ), by:
+            </p>
 
-              <p>
-                <b>Albert Tian Chen</b> is an associate computational biologist
-                in the Deverman group who designed, built, and will continue to
-                expand the COVID-19 CoV Genetics browser. Albert has previous
-                experience building websites for startups and interactive data
-                visualization tools for research groups.
-              </p>
-              <p>
-                <b>Kevin Altschuler</b> is an experienced web developer with
-                previous positions at Google and Uber Advanced Technologies
-                Group (ATG). Kevin has architected the application to streamline
-                development and improve performance.
-              </p>
-              <p>
-                <b>Dr. Alina Chan</b> is a postdoc in the Deverman group who
-                asked too many questions about SARS-CoV-2 and realized that we
-                needed a browser like COVID-19 CG.
-              </p>
-              <p>
-                <b>Dr. Shing Hei Zhan</b> is a recent graduate from the
-                University of British Columbia’s Department of Zoology &amp; the
-                Biodiversity Research Centre. Shing is also co-founder and lead
-                bioinformatics scientist at Fusion Genomics Corporation, which
-                develops molecular diagnostic assays for infectious diseases,
-                including COVID-19.
-              </p>
-              <p>
-                <b>Dr. Benjamin Deverman</b> is the director of the vector
-                engineering research group at the Stanley Center for Psychiatric
-                Research at the Broad Institute of MIT and Harvard. The Deverman
-                Lab develops innovative gene delivery solutions to expand the
-                impact of gene therapy and for studying the central nervous
-                system, with the aim of uncovering new avenues for treating
-                psychiatric disorders.
-              </p>
+            <p>
+              <b>Albert Tian Chen</b> is an associate computational biologist in
+              the Deverman group who designed, built, and will continue to
+              expand the COVID-19 CoV Genetics browser. Albert has previous
+              experience building websites for startups and interactive data
+              visualization tools for research groups.
+            </p>
+            <p>
+              <b>Kevin Altschuler</b> is an experienced web developer with
+              previous positions at Google and Uber Advanced Technologies Group
+              (ATG). Kevin has architected the application to streamline
+              development and improve performance.
+            </p>
+            <p>
+              <b>Dr. Alina Chan</b> is a postdoc in the Deverman group who asked
+              too many questions about SARS-CoV-2 and realized that we needed a
+              browser like COVID-19 CG.
+            </p>
+            <p>
+              <b>Dr. Shing Hei Zhan</b> is a recent graduate from the University
+              of British Columbia’s Department of Zoology &amp; the Biodiversity
+              Research Centre. Shing is also co-founder and lead bioinformatics
+              scientist at Fusion Genomics Corporation, which develops molecular
+              diagnostic assays for infectious diseases, including COVID-19.
+            </p>
+            <p>
+              <b>Dr. Benjamin Deverman</b> is the director of the vector
+              engineering research group at the Stanley Center for Psychiatric
+              Research at the Broad Institute of MIT and Harvard. The Deverman
+              Lab develops innovative gene delivery solutions to expand the
+              impact of gene therapy and for studying the central nervous
+              system, with the aim of uncovering new avenues for treating
+              psychiatric disorders.
+            </p>
 
-              <p>
-                Contact the authors by email:{' '}
-                <a href="mailto:covidcg@broadinstitute.org">
-                  covidcg@broadinstitute.org
-                </a>
-              </p>
-            </div>
-            <div className="content-images">
-              <ImageRow>
-                <ExternalLink href="https://vector.engineering">
-                  <img src={VELogo} height="40"></img>
-                </ExternalLink>
-                <VELogoText>Vector Engineering Lab</VELogoText>
-              </ImageRow>
-              <ImageRow>
-                <ExternalLink href="https://www.broadinstitute.org/">
-                  <img
-                    src={BroadLogo}
-                    height="40"
-                    style={{ marginRight: '10px' }}
-                  ></img>
-                </ExternalLink>
-                {/* <ExternalLink href="https://www.broadinstitute.org/stanley">
+            <p>
+              Contact the authors by email:{' '}
+              <a href="mailto:covidcg@broadinstitute.org">
+                covidcg@broadinstitute.org
+              </a>
+            </p>
+
+            <ImageRow>
+              <ExternalLink href="https://vector.engineering">
+                <img src={VELogo} height="40"></img>
+              </ExternalLink>
+              <VELogoText>Vector Engineering Lab</VELogoText>
+              <ExternalLink href="https://www.broadinstitute.org/">
+                <img
+                  src={BroadLogo}
+                  height="40"
+                  style={{ marginLeft: '20px' }}
+                ></img>
+              </ExternalLink>
+            </ImageRow>
+            <ImageRow>
+              {/* <ExternalLink href="https://www.broadinstitute.org/stanley">
                   <img
                     src="https://storage.googleapis.com/ve-public/covid_ui/assets/img/StanleyCenterLogo_RGB_forDigital.png"
                     height="50"
                   ></img>
                 </ExternalLink> */}
-              </ImageRow>
-              {/* <ImageRow>
+            </ImageRow>
+            {/* <ImageRow>
                 <ExternalLink href="https://www.ubc.ca/">
                   <img src={UBCLogo} height="60"></img>
                 </ExternalLink>
               </ImageRow> */}
-            </div>
-          </div>
-        </ContentSection>
-
-        <ContentSection>
-          <a id="citing-covid-cg"></a>
-          <span className="section-title">Citing COVID-19 CG:</span>
-          <div className="content-block">
-            <div className="content-text">
-              {/* <p>
-                Chen AT, Altschuler K, Chan AY, Zhan SH, Deverman BE (2020).
-                COVID-19 CG: Tracking SARS-CoV-2 by mutation, location, and date
-                of interest. <i>bioRxiv</i>. DOI: ...
-              </p> */}
-              <p>
-                Users are encouraged to share, download, and further analyze
-                data from this site. Plots can be downloaded as PNG or SVG
-                files, and the data powering the plots and tables can be
-                downloaded as well. Please attribute any data/images to{' '}
-                <a href="https://covidcg.org">covidcg.org</a>.
-              </p>
-            </div>
-            <div className="content-images"></div>
           </div>
         </ContentSection>
 
@@ -162,86 +136,99 @@ const AboutTab = () => {
           <a id="sequence-data"></a>
           <span className="section-title">Data enabling COVID-CG</span>
 
-          <div className="content-block">
-            <div className="content-text">
-              <p>
-                We are extremely grateful to the{' '}
-                <ExternalLink href="https://www.gisaid.org/">
-                  GISAID Initiative
-                </ExternalLink>{' '}
-                and all its data contributors, i.e. the Authors from the
-                Originating laboratories responsible for obtaining the
-                speciments and the Submitting laboratories where genetic
-                sequence data were generated and shared via the GISAID
-                Initiative, on which this research is based.
-              </p>
-              <p>
-                Elbe, S., and Buckland-Merrett, G. (2017) Data, disease and
-                diplomacy: GISAID’s innovative contribution to global health.
-                Global Challenges, 1:33-46. DOI:
-                <ExternalLink href="https://doi.org/10.1002/gch2.1018">
-                  10.1002/gch2.1018
-                </ExternalLink>{' '}
-                PMCID:{' '}
-                <ExternalLink href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6607375/">
-                  31565258
-                </ExternalLink>
-              </p>
-              <p>
-                Note: When using results from these analyses in your manuscript,
-                ensure that you acknowledge the contributors of data, i.e.{' '}
-                <i>
-                  We gratefully acknowledge all the Authors from the Originating
-                  laboratories responsible for obtaining the speciments and the
-                  Submitting laboratories where genetic sequence data were
-                  generated and shared via the GISAID Initiative, on which this
-                  research is based.
-                </i>
-              </p>
-              <p>and cite the following reference:</p>
-              <p>
-                Shu, Y., McCauley, J. (2017) GISAID: Global initiative on
-                sharing all influenza data – from vision to reality.
-                EuroSurveillance, 22(13) DOI:
-                <ExternalLink href="https://doi.org/10.2807/1560-7917.ES.2017.22.13.30494">
-                  10.2807/1560-7917.ES.2017.22.13.30494
-                </ExternalLink>{' '}
-                PMCID:{' '}
-                <ExternalLink href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5388101/">
-                  PMC5388101
-                </ExternalLink>
-              </p>
-            </div>
-            <div className="content-images">
+          <div className="content-text">
+            <p>
+              We are extremely grateful to the{' '}
               <ExternalLink href="https://www.gisaid.org/">
-                <img src={GISAIDLogo} height="60"></img>
+                GISAID Initiative
+              </ExternalLink>{' '}
+              and all its data contributors, i.e. the Authors from the
+              Originating laboratories responsible for obtaining the speciments
+              and the Submitting laboratories where genetic sequence data were
+              generated and shared via the GISAID Initiative, on which this
+              research is based.
+            </p>
+            <ImageRow>
+              <ExternalLink href="https://gisaid.org">
+                <img src={GISAIDLogo} height="60" />
               </ExternalLink>
-            </div>
+            </ImageRow>
           </div>
         </ContentSection>
 
         <ContentSection>
+          <a id="citing-covid-cg"></a>
+          <span className="section-title">Citing COVID-19 CG:</span>
+          <div className="content-text">
+            {/* <p>
+                Chen AT, Altschuler K, Chan AY, Zhan SH, Deverman BE (2020).
+                COVID-19 CG: Tracking SARS-CoV-2 by mutation, location, and date
+                of interest. <i>bioRxiv</i>. DOI: ...
+              </p> */}
+            <p>
+              Users are encouraged to share, download, and further analyze data
+              from this site. Plots can be downloaded as PNG or SVG files, and
+              the data powering the plots and tables can be downloaded as well.
+              Please attribute any data/images to{' '}
+              <a href="https://covidcg.org">covidcg.org</a>.
+            </p>
+            <p>
+              Note: When using results from these analyses in your manuscript,
+              ensure that you acknowledge the contributors of data, i.e.{' '}
+              <i>
+                We gratefully acknowledge all the Authors from the Originating
+                laboratories responsible for obtaining the speciments and the
+                Submitting laboratories where genetic sequence data were
+                generated and shared via the GISAID Initiative, on which this
+                research is based.
+              </i>
+            </p>
+            <p>and cite the following reference(s):</p>
+            <p>
+              Shu, Y., McCauley, J. (2017) GISAID: Global initiative on sharing
+              all influenza data – from vision to reality.{' '}
+              <i>EuroSurveillance</i>, 22(13) DOI:
+              <ExternalLink href="https://doi.org/10.2807/1560-7917.ES.2017.22.13.30494">
+                10.2807/1560-7917.ES.2017.22.13.30494
+              </ExternalLink>{' '}
+              PMCID:{' '}
+              <ExternalLink href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5388101/">
+                PMC5388101
+              </ExternalLink>
+            </p>
+            <p>
+              Elbe, S., and Buckland-Merrett, G. (2017) Data, disease and
+              diplomacy: GISAID’s innovative contribution to global health.{' '}
+              <i>Global Challenges</i>, 1:33-46. DOI:
+              <ExternalLink href="https://doi.org/10.1002/gch2.1018">
+                10.1002/gch2.1018
+              </ExternalLink>{' '}
+              PMCID:{' '}
+              <ExternalLink href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6607375/">
+                31565258
+              </ExternalLink>
+            </p>
+          </div>
+        </ContentSection>
+
+        {/* <ContentSection>
           <a id="reference-data"></a>
           <span className="section-title">Reference Sequence</span>
 
           <div className="content-block">
             <div className="content-text">
               <p>
-                All hCoV-19 sequences are aligned to the Wuhan-Hu-1 reference
-                sequence (
-                <ExternalLink href="https://www.ncbi.nlm.nih.gov/nuccore/NC_045512.2">
-                  NCBI: NC_045512.2
-                </ExternalLink>
-                , &quot;WH01&quot; in Lu et al., 2020, DOI:{' '}
-                <ExternalLink href="https://doi.org/10.1016/S0140-6736(20)30251-8">
-                  10.1016/S0140-6736(20)30251-8
+                All hCoV-19 sequences are aligned to the WIV04 reference
+                sequence (GISAID: EPI_ISL_402124,
+                <ExternalLink href="https://www.ncbi.nlm.nih.gov/nuccore/MN996528">
+                  NCBI: MN996528.1
                 </ExternalLink>
                 ). Gene and protein ORFs were taken from the GenBank page.
               </p>
             </div>
             <div className="content-images"></div>
           </div>
-        </ContentSection>
+        </ContentSection> */}
         {/*
         <ContentSection>
           <a id="primer-data"></a>
