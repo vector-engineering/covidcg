37bf1f8f71ccb0d2cff67a641cde6c1f79216a62
Albert Chen <chena@broadinstitute.org>
2020-08-27 10:48:19 -0400

    fix crash after deselecting location on compare locations tab

--- src/components/Vega/Plot.styles.js
+++ src/components/Vega/Plot.styles.js
@@ -44,3 +44,40 @@ export const PlotTitle = styled.div`
     font-weight: normal;
   }
 `;
+
+export const WarningContainer = styled.div`
+  display: ${({ show }) => (show ? 'flex' : 'none')};
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: flex-start;
+
+  // colors from Bootstrap
+  background-color: #fff3cd;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+
+  margin: 0px 10px;
+  margin-bottom: 15px;
+  padding: 10px 20px;
+
+  .warning-header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 5px;
+    .warning-title {
+      font-size: 1.25em;
+    }
+    .spacer {
+      flex-grow: 1;
+    }
+  }
+
+  .warning-text {
+    margin: 0px;
+    font-weight: normal;
+  }
+`;
+WarningContainer.defaultProps = {
+  show: true,
+};
