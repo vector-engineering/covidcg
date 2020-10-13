d8aaa1cb85f27fcdf7186ed191e90d48f8f4aff7
kevinaltschuler <kevinaltschuler@gmail.com>
2020-10-12 21:22:32 -0400

    fade in on the splash screen

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { Transition } from 'react-transition-group';
 import { useStores } from '../stores/connect';
 import { asyncDataStoreInstance } from './App';
 
@@ -15,6 +16,34 @@ import {
 
 import CGLogo from '../assets/images/cg_logo_v13.svg';
 
+const duration = 1000;
+
+const defaultStyle = {
+  transition: `all ${duration}ms ease-in-out`,
+  opacity: 0,
+  transform: 'scale(.8, .8)',
+};
+
+const transitionStyles = {
+  entering: { opacity: 0, transform: 'scale(.8, .8)' },
+  entered: { opacity: 1, transform: 'scale(1, 1)' },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
+
+const loadingDefaultStyles = {
+  transition: `all 1000ms ease-in-out`,
+  opacity: 0,
+  transform: 'scale(.8, .8)',
+};
+
+const loadingTransitionStyles = {
+  entering: { opacity: 0, transform: 'scale(.8, .8)' },
+  entered: { opacity: 1, transform: 'scale(1, 1)' },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
+
 const LoadingScreen = observer(() => {
   const { UIStore } = useStores();
 
@@ -27,13 +56,31 @@ const LoadingScreen = observer(() => {
 
   return (
     <LoadingScreenContainer>
-      <LogoContainer>
-        <img src={CGLogo}></img>
-      </LogoContainer>
-      <ProgressContainer>
-        <LoadingSpinner />
-        <ProgressText>{progressText}</ProgressText>
-      </ProgressContainer>
+      <Transition in appear enter timeout={duration}>
+        {(state) => (
+          <LogoContainer
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <img src={CGLogo}></img>
+          </LogoContainer>
+        )}
+      </Transition>
+      <Transition in appear enter timeout={300}>
+        {(state) => (
+          <ProgressContainer
+            style={{
+              ...loadingDefaultStyles,
+              ...loadingTransitionStyles[state],
+            }}
+          >
+            <LoadingSpinner />
+            <ProgressText>{progressText}</ProgressText>
+          </ProgressContainer>
+        )}
+      </Transition>
     </LoadingScreenContainer>
   );
 });
