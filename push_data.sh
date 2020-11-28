bd45e5a034f1d347ba7ee2b696470abd77b3aec1
Albert Tian Chen <chena@broadinstitute.org>
2020-11-28 18:01:32 -0500

    exclude some files from upload

--- push_data.sh
+++ push_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rclone copy data "shared:VE computational/covid_data" -P --transfers=20 --checkers=20
+rclone copy data "shared:VE computational/covid_data" -P --transfers=20 --checkers=20 --exclude "/data/feed.json" --exclude "/data/fasta_temp/**"
 
 # Push package to google cloud storage
 gsutil -h "Cache-Control: no-cache" cp data/data_package.json gs://ve-public/data_package.json
