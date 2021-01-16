3ec46c12ac797641364047814ce960a278dcfd6e
Albert Tian Chen <chena@broadinstitute.org>
2021-01-16 17:34:11 -0500

    working rclone exclusions

--- push_data.sh
+++ push_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rclone copy data "mydrive:covid_data" --transfers=10 -P
+rclone copy --exclude "fasta_temp/**" data "mydrive:covid_data" --transfers=10 -P 
 
 # Push package to google cloud storage
-gsutil -h "Cache-Control: no-cache" cp data/data_package.json.gz gs://ve-public/data_package.json.gz
+gsutil -h "Cache-Control: no-cache" cp data/data_package.json.gz gs://ve-public/v1.4/data_package.json.gz
 gsutil -h "Cache-Control: no-cache" cp data/map_combined_standalone.vg.json gs://ve-public/map_combined_standalone.vg.json
 
 # Push genbank data
-rclone copy data_genbank "mydrive:covid_data/genbank" --transfers 10 -P
+rclone copy --exclude "fasta_temp/**" data_genbank "mydrive:covid_data/genbank" --transfers 10 -P
 
 # Push genbank packages
 gsutil -h "Cache-Control: no-cache" cp data_genbank/data_package.json.gz gs://ve-public/genbank/data_package.json.gz
