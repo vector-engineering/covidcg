94e18411848900b96a300846737b967243ebac90
Albert Tian Chen <chena@broadinstitute.org>
2021-01-19 03:57:38 -0500

    Update data endpoint

--- push_data.sh
+++ push_data.sh
@@ -3,7 +3,7 @@
 rclone copy --exclude "fasta_temp/**" data "mydrive:covid_data" --transfers=10 -P 
 
 # Push package to google cloud storage
-gsutil -h "Cache-Control: no-cache" cp data/data_package.json.gz gs://ve-public/v1.4/data_package.json.gz
+gsutil -h "Cache-Control: no-cache" cp data/data_package.json.gz gs://ve-public/v1.5/data_package.json.gz
 gsutil -h "Cache-Control: no-cache" cp data/map_combined_standalone.vg.json gs://ve-public/map_combined_standalone.vg.json
 
 # Push genbank data
