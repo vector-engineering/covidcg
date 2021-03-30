3e9b203560e5de2c3c5d87ed266119b53b125706
Albert Chen <chena@broadinstitute.org>
2021-03-29 21:11:08 -0400

    build script for all 4 site versions

--- build/build.sh
+++ build/build.sh
@@ -7,10 +7,7 @@ else
     echo "Building version: ${CG_VERSION}"
 fi
 
-gcloud builds submit \
-    --config build/cloudbuild.yaml \
-    --substitutions=_TARGET="cg",_CONFIGFILE="config/config_gisaid.yaml",_TAG_NAME="${CG_VERSION}", . && \
-gcloud builds submit \
-    --config build/cloudbuild.yaml \
-    --substitutions=_TARGET="cg",_CONFIGFILE="config/config_gisaid.yaml",_TAG_NAME="${CG_VERSION}" . && \
+gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg",_CONFIGFILE="config/config_gisaid.yaml",_TAG_NAME="${CG_VERSION}", . && \
+gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg-genbank",_CONFIGFILE="config/config_genbank.yaml",_TAG_NAME="${CG_VERSION}" . && \
 gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg-private",_CONFIGFILE="config/config_gisaid_private.yaml",_TAG_NAME="${CG_VERSION}" .
+gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg-alpha",_CONFIGFILE="config/config_alpha.yaml",_TAG_NAME="${CG_VERSION}", .
