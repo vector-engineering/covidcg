746c2256e4921436d89c21df183aebe9861f6da9
Albert Chen <chena@broadinstitute.org>
2021-04-08 16:41:04 -0400

    add example

--- src/utils/primer.js
+++ src/utils/primer.js
@@ -43,6 +43,7 @@ export function getPrimersByGroup(groupName) {
   return _.where(processedPrimers, { Institution: groupName });
 }
 
+// Example usage: queryPrimers({ Institution: "CDC", Name: "N1" })
 export function queryPrimers(queryObj) {
   return _.where(processedPrimers, queryObj);
 }
