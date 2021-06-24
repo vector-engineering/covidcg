bf5033a49a130cb4d6c5691a5bc7d9637aa078a3
Albert Tian Chen <chena@broadinstitute.org>
2021-06-24 06:18:47 -0400

    v2.1.0 beta1 (#341)
    * bump versions
    
    * fix log(0) bug
    
    * refactor examples
    
    * add paper banner to home tab (closes #326)
    
    * fix axis in global sequencing homepage cases panel
    
    * add Delta variant to default selected list

---
+++ src/components/Pages/HomeTab.styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const HomeTabContainer = styled.div`
+  background-color: #f8f8f8;
+`;
+
+export const HomeTabContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  flex-grow: 1;
+  padding: 20px;
+  margin: 0 auto;
+
+  background-color: #fff;
+`;
+
+export const PubBanner = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: #fff3cd;
+  padding: 5px;
+  border-bottom: 1px solid #aaa;
+
+  p {
+    margin: 0px;
+    margin-left: auto;
+  }
+`;
+
+export const CloseButton = styled.button`
+  margin-left: auto;
+`;
