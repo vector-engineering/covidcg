c4f7d343189274cdb7b4808ea770b12b13656436
Albert Tian Chen <chena@broadinstitute.org>
2021-06-24 05:26:50 -0400

    add additional fields to lineage tree

--- workflow_main/phylotree/scripts/representative_seqs.py
+++ workflow_main/phylotree/scripts/representative_seqs.py
@@ -12,22 +12,11 @@ Author: Albert Chen - Vector Engineering Team (chena@broadinstitute.org)
 """
 
 import pandas as pd
-
-# import numpy as np
+import numpy as np
 import json
 
-# import gzip
-# import os
-# import re
-# import subprocess
-
 from datetime import date
-
-
-# from io import StringIO
 from tempfile import NamedTemporaryFile
-
-# from pathlib import Path
 
 
 def date2float(isodate):
@@ -42,24 +31,64 @@ def date2float(isodate):
 def get_representative_seqs(
     case_data_path,
     metadata_map_path,
+    location_map_path,
     ref_seq_path,
     fasta_out_path,
     datefile_out_path,
     table_out_path,
 ):
 
-    # Load data and select a representative sequence for each lineage
-    # by choosing the last seen sequence from each lineage
-    df = pd.read_json(case_data_path)
-    reps = df.drop_duplicates("lineage", keep="last")[
-        ["Accession ID", "collection_date", "lineage", "dna_snp_str"]
-    ].set_index("Accession ID")
-
     # Load DNA mutation ID map
     with open(metadata_map_path, "r") as fp:
         metadata_map = json.loads(fp.read())
     dna_snp = metadata_map["dna_snp"]
     id_to_dna_snp = {v: k for k, v in dna_snp.items()}
+
+    # Load location map
+    location_map = pd.read_json(location_map_path)
+
+    # Load data and select a representative sequence for each lineage
+    # by choosing the last seen sequence from each lineage
+    df = pd.read_json(case_data_path)
+    df["collection_date"] = pd.to_datetime(df["collection_date"])
+    df = df.join(location_map, on="location_id")
+
+    reps = (
+        df.loc[
+            (df["collection_date"] >= pd.to_datetime("2019-12-15"))
+            & (df["collection_date"] <= pd.to_datetime(date.today()))
+        ]
+        .drop_duplicates("lineage", keep="last")[
+            ["Accession ID", "collection_date", "lineage", "dna_snp_str"]
+        ]
+        .set_index("Accession ID")
+        # Join first and last collection dates
+        .join(
+            (
+                df.loc[
+                    (df["collection_date"] > pd.to_datetime("2019-12-15"))
+                    & (df["collection_date"] < pd.to_datetime(date.today()))
+                ]
+                .groupby("lineage")
+                .agg(
+                    date_min=("collection_date", np.min),
+                    date_max=("collection_date", np.max),
+                )
+            ),
+            on="lineage",
+        )
+        # Join region with the most counts of this lineage
+        .join(
+            (
+                df.groupby(["lineage", "region"], as_index=False)[["Accession ID"]]
+                .agg("count")
+                .groupby("lineage")
+                .apply(lambda x: x["region"].values[np.argmax(x["Accession ID"])])
+                .rename("region_most_common")
+            ),
+            on="lineage",
+        )
+    )
 
     # Load reference sequence
     with open(ref_seq_path, "r") as fp:
@@ -100,6 +129,7 @@ def get_representative_seqs(
 
     # Write files for treetime
     # Extract dates from sequence headers
+    reps["collection_date"] = reps["collection_date"].astype(str)
     with open(datefile_out_path, "w") as fp:
         fp.write("name,date\n")
         for accession_id, row in reps.iterrows():
@@ -108,4 +138,4 @@ def get_representative_seqs(
         fp.close()
 
     # Write dataframe for future
-    reps.to_csv(table_out_path)
+    reps.drop(columns=["dna_snp_str"]).to_csv(table_out_path)
