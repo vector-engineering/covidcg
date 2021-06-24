bd601883156efcc10f789d31fa99ca2a0c9ca58a
Albert Tian Chen <chena@broadinstitute.org>
2021-06-24 05:27:49 -0400

    add additional fields to lineage tree (#340)

--- workflow_main/phylotree/scripts/build_graph_table.py
+++ workflow_main/phylotree/scripts/build_graph_table.py
@@ -18,7 +18,11 @@ from Bio import Phylo
 
 
 def build_graph_table(
-    case_data_path, nexus_file_path, representative_table_path, graph_table_out_path
+    case_data_path,
+    nexus_file_path,
+    tree_dates_path,
+    representative_table_path,
+    graph_table_out_path,
 ):
     df = pd.read_json(case_data_path)
 
@@ -28,11 +32,10 @@ def build_graph_table(
 
     reps = pd.read_csv(representative_table_path, index_col="Accession ID")
 
-    nodes = pd.DataFrame(
-        [(node.name, node.branch_length) for node in net.nodes],
-        columns=["name", "branch_length"],
+    nodes = pd.DataFrame([(node.name,) for node in net.nodes], columns=["name",],)
+    nodes = nodes.join(
+        reps[["lineage", "date_min", "date_max", "region_most_common"]], on="name"
     )
-    nodes = nodes.join(reps[["lineage"]], on="name")
     nodes["leaf"] = ~nodes["lineage"].isna()
 
     edges = pd.DataFrame(
@@ -41,19 +44,6 @@ def build_graph_table(
     )
 
     nodes = nodes.set_index("name").join(edges.set_index("child"))
-    nodes["cumulative_length"] = nodes["branch_length"]
-
-    # Recursively add length to children
-    def add_length(node_name):
-        children = nodes.loc[nodes["parent"] == node_name]
-
-        for child_name, child in children.iterrows():
-            nodes.loc[child_name, "cumulative_length"] += nodes.loc[
-                node_name, "cumulative_length"
-            ]
-            add_length(child_name)
-
-    add_length("NODE_0000001")
 
     # Recursively add counts to children
     counts_per_lineage = df["lineage"].value_counts()
@@ -70,5 +60,9 @@ def build_graph_table(
         return num_seqs
 
     add_seqs_to_parent("NODE_0000001")
+
+    # Add divergence dates
+    dates = pd.read_csv(tree_dates_path, sep="\t")
+    nodes = nodes.join(dates.set_index("#node")[["date"]])
 
     nodes.reset_index().to_json(graph_table_out_path, orient="records")
