d25e1a95172a8d26e6d0613e9839ca8220f0f6b7
Albert Chen <chena@broadinstitute.org>
2021-07-29 14:54:59 -0400

    Specify start and end dates for examples

--- src/utils/date.js
+++ src/utils/date.js
@@ -7,3 +7,7 @@ export function intToISO(dateNum) {
 export function ISOToInt(dateStr) {
   return new Date(dateStr).getTime();
 }
+
+export function todayISO() {
+  return intToISO(new Date().getTime());
+}
