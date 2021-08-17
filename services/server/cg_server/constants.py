82fa5479fba601b4aa4e86a7dd5d7c7e3dc1c788
Albert Chen <chena@broadinstitute.org>
2021-08-17 13:36:07 -0400

    Add env var for constants file, and better default that works on per-service deployments (#381)

--- services/server/cg_server/constants.py
+++ services/server/cg_server/constants.py
@@ -6,10 +6,19 @@ Author: Albert Chen - Vector Engineering Team (chena@broadinstitute.org)
 """
 
 import json
+import os
+
+from pathlib import Path
 
 constants = {}
 
+constants_path = os.getenv(
+    "CONSTANTSFILE",
+    # root/services/server/cg_server/constants.py --> need to go back 3 levels
+    Path(__file__).parent.parent.parent.parent / "src" / "constants" / "defs.json",
+)
+
 # Load constant defs
-with open("/opt/constants/defs.json", "r") as fp:
+with open(constants_path, "r") as fp:
     constants = json.loads(fp.read())
 
