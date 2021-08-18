949c47f92b6496f6bf5da1d4ef2124b31694d48b
favelava <favela.david96@gmail.com>
2021-08-18 13:10:54 -0400

    Updated to work with new CDC layout

--- workflow_main/scripts/get_cdc_vocs.py
+++ workflow_main/scripts/get_cdc_vocs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import re
 import argparse
 import json
 import requests
@@ -12,6 +13,8 @@ def get_cdc_vocs():
            'variants/variant-info.html')
     level_order = ['VOI', 'VOC']
     variant_list = []
+
+    lineage_pattern = re.compile('([A-Z]+([.]+\\d+)+([.]+\\d+))')
 
     vocPage = requests.get(url)
     soup = BeautifulSoup(vocPage.content, 'html.parser')
@@ -28,28 +31,16 @@ def get_cdc_vocs():
         rowgroup = table.div
         rows = rowgroup.find_all('div', class_='col-md-12 p-md-3 pt-0 pb-3')
         for row in rows:
-            variants = list(row.stripped_strings)
-            variant = {'name': variants[0],
-                       'level': level}
-            variant_list.append(variant)
+            variant_row = list(row.stripped_strings)
+            for col in variant_row:
+                for match in lineage_pattern.findall(col):
+                    print(match)
+                    variant = {'name': match[0],
+                               'level': level}
+                    variant_list.append(variant)
         level_ind += 1
 
-    return variant_list
+    print(variant_list)
 
 
-def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-o", "--output", required=True, type=str,
-                        help="Path to output file")
-
-    args = parser.parse_args()
-
-    variant_list = get_cdc_vocs()
-
-    with open(args.output, 'w') as fp:
-        fp.write(json.dumps(variant_list, indent=2))
-
-
-if __name__ == "__main__":
-    main()
+get_cdc_vocs()
