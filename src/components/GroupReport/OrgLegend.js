e047bb24d0715f850f2de18c30249f640cac70a1
favelava <favela.david96@gmail.com>
2021-08-19 10:09:34 -0400

    A tooltip explaining the VOCList has been added

--- src/components/GroupReport/OrgLegend.js
+++ src/components/GroupReport/OrgLegend.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { colors } from './VOCList';
 
+import QuestionButton from '../Buttons/QuestionButton';
+
 import {
   OrgLegendContainer,
   OrgLegendTitle,
@@ -34,6 +36,19 @@ export const OrgLegend = () => {
     <OrgLegendContainer>
       <OrgLegendTitle>Classified By</OrgLegendTitle>
       <OrgItemContainer>{orgs}</OrgItemContainer>
+      <QuestionButton
+        data-tip={`
+          <div>
+          <p>Displayed below are the PANGO lineages of variants being specifically monitored by at least one of these organizations.</p>
+          <p>Variants of Concern are being most closely monitored, followed by Variants of Interest (called Variants Under Investigation by the UK PHE).</p>
+          <p>Any other, organization-specific classifications are collectively grouped under 'Other Variants Being Monitored'.</p>
+          <p>If a square next to a lineage is colored in, that organization has given the lineage whichever classification it is below.</p>
+          <p>(i.e. B.1.617.2 has been labeled a Variant of Concern by the WHO, CDC, ECDC, and PHE).</p>
+          </div>
+        `}
+        data-html="true"
+        data-for="main-tooltip"
+      />
     </OrgLegendContainer>
   );
 };
