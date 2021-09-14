dac78d557a9d7fd4a2446d8293ed7b4ef26c553c
favelava <favela.david96@gmail.com>
2021-09-14 14:19:50 -0400

    Genbank data ingest working for rsv

--- workflow_rsv_genbank_ingest/scripts/copy_changed_files.py
+++ workflow_rsv_genbank_ingest/scripts/copy_changed_files.py
@@ -10,7 +10,7 @@ import shutil
 from pathlib import Path
 
 
-def copy_changed_files(files, data_folder):
+def copy_changed_files(path, data_folder):
     """Using the `get_changed_chunks` function, only copy fasta files which have changed
     from the purgatory `fasta_temp` folder to the `fasta_raw` folder. By copying over the files,
     it will flag to snakemake that they (and only they - not the others) will need to be
@@ -18,7 +18,7 @@ def copy_changed_files(files, data_folder):
 
     Parameters
     ----------
-    files: list of str
+    path: path to txt file containing list of str
     data_folder: str
 
     Returns
@@ -32,10 +32,13 @@ def copy_changed_files(files, data_folder):
 
     # For each changed chunk (as defined by `get_changed_chunks`),
     # copy over the fasta file from the `fasta_temp` folder to the `fasta_raw` folder
-    for chunk in files:
-        chunk = re.sub(r"\.fa\.gz$", "", Path(chunk).name)
-        fasta_temp_path = Path(data_folder) / "fasta_temp" / (chunk + ".fa.gz")
-        fasta_raw_path = Path(data_folder) / "fasta_raw" / (chunk + ".fa.gz")
+    with open(path, 'r') as f:
+        files = f.readlines()
+        for chunk in files:
+            chunk = chunk.rstrip()
+            chunk = re.sub(r"\.fa\.gz$", "", Path(chunk).name)
+            fasta_temp_path = Path(data_folder) / "fasta_temp" / (chunk + ".fa.gz")
+            fasta_raw_path = Path(data_folder) / "fasta_raw" / (chunk + ".fa.gz")
 
-        shutil.copyfile(fasta_temp_path, fasta_raw_path)
-        shutil.copystat(fasta_temp_path, fasta_raw_path)
+            shutil.copyfile(fasta_temp_path, fasta_raw_path)
+            shutil.copystat(fasta_temp_path, fasta_raw_path)
