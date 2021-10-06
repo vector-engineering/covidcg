317de01b46d1f6fab6260b79a72aed8b1e7a551a
Alex Klementovich <a13k13m@gmail.com>
2021-10-06 10:51:10 -0400

    Albert the all-knowing

--- src/components/Buttons/QuestionButton.js
+++ src/components/Buttons/QuestionButton.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 const QuestionButtonContainer = ({ ...props }) => {
   useEffect(() => {
     // This prevents every single QuestionButton from rebuilding if they aren't visible.
-    if (props.rebuild === 'rebuild') {
+    if (props.rebuildAfterMount) {
       ReactTooltip.rebuild();
     }
   });
