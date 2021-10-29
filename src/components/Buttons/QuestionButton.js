dc9a6ccd4db57c065fe59ac0d5496f9fe7d64480
favelava <favela.david96@gmail.com>
2021-10-29 11:48:29 -0400

    Changed rebuildAfterMount to supplied prop rebuild

--- src/components/Buttons/QuestionButton.js
+++ src/components/Buttons/QuestionButton.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 const QuestionButtonContainer = ({ ...props }) => {
   useEffect(() => {
     // This prevents every single QuestionButton from rebuilding if they aren't visible.
-    if (props.rebuildAfterMount) {
+    if (props.rebuild !== 'false') {
       ReactTooltip.rebuild();
     }
   }, []);
