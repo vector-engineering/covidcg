9dc6623d0236de0626b515fb561011321a0042c2
Albert Chen <chena@broadinstitute.org>
2021-11-12 00:26:34 -0500

    More conventional server structure

--- services/server/cg_server/query/__init__.py
+++ services/server/cg_server/query/__init__.py
@@ -5,14 +5,10 @@
 Author: Albert Chen - Vector Engineering Team (chena@broadinstitute.org)
 """
 
-from .country_score import query_country_score
-from .group_mutation_frequencies import query_group_mutation_frequencies
-from .initial import query_initial
-from .selection import (
-    query_and_aggregate,
-    build_sequence_where_filter,
-    build_sequence_location_where_filter,
-)
-from .metadata import query_metadata
-from .report import generate_report
+from .country_score import *
+from .group_mutation_frequencies import *
+from .initial import *
+from .selection import *
+from .metadata import *
+from .report import *
 
