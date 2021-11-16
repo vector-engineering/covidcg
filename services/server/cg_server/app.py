c60b2776dc81e6041652611f63899e2f1d34b308
favelava <64983484+favelava@users.noreply.github.com>
2021-11-16 16:34:26 -0500

    Fixes bug where init never resolves (#441)

--- services/server/cg_server/app.py
+++ services/server/cg_server/app.py
@@ -7,7 +7,9 @@ Author: Albert Chen - Vector Engineering Team (chena@broadinstitute.org)
 
 import os
 
-from cg_server.config import config
+from cg_server.config import config, project_root
+from cg_server.db_seed import seed_database, insert_sequences
+from cg_server.db import get_conn_from_pool
 
 from flask import Flask
 from flask_gzip import Gzip
@@ -26,3 +28,30 @@ if os.getenv("FLASK_ENV", "development") == "development":
 # CORS config
 CORS(app, origins=cors_domains)
 
+# Quickly check if our database has been initialized yet
+# If not, then let's seed it
+# Only allow in development mode
+if os.getenv("FLASK_ENV", "development") == "development":
+    conn = get_conn_from_pool()
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            SELECT EXISTS (
+                SELECT FROM pg_tables
+                WHERE  schemaname = 'public'
+                AND    tablename  = 'metadata'
+            );
+            """
+        )
+        exists = cur.fetchone()[0]
+
+    if not exists:
+        print("Seeding DB")
+        seed_database(conn)
+
+        insert_sequences(
+            conn,
+            os.getenv("DATA_PATH", project_root / config["data_folder"]),
+            filenames_as_dates=True,
+        )
+        conn.commit()
