575561062b4d9281d48ba89b49efd71868d57ca6
Albert Chen <chena@broadinstitute.org>
2022-01-18 18:36:02 -0500

    Mutation formatting - stop codons as *, deletions with delta, and insertions without missing char

--- src/utils/mutationUtils.js
+++ src/utils/mutationUtils.js
@@ -8,10 +8,35 @@ export const formatMutation = (mutationStr, dnaOrAa) => {
 
   // Print as REF POS ALT
   // i.e., 23403|A|G -> A23403G, S|614|D|G -> S:D614G
+
   const chunks = mutationStr.split('|');
   if (dnaOrAa === DNA_OR_AA.DNA) {
     return `${chunks[1]}${chunks[0]}${chunks[2]}`;
   } else if (dnaOrAa === DNA_OR_AA.AA) {
-    return `${chunks[0]}:${chunks[2]}${chunks[1]}${chunks[3]}`;
+    let ref = chunks[2];
+    let alt = chunks[3];
+
+    // Translate stop codon from "_" to more conventional "*"
+    if (ref === '_') {
+      ref = '*';
+    }
+    if (alt === '_') {
+      alt = '*';
+    }
+
+    // For deletions, format as ∆[ref][pos]
+    // i.e., F13- --> ∆F13
+    if (alt === '-') {
+      alt = '';
+      ref = '∆' + ref;
+    }
+
+    // For insertions, remove ref '-'
+    // i.e., -13F --> 13F
+    if (ref === '-') {
+      ref = '';
+    }
+
+    return `${chunks[0]}:${ref}${chunks[1]}${alt}`;
   }
 };
