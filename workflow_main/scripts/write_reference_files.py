07a1e8580800a55ecb2a3c5f408f80094c1efebb
Albert Tian Chen <chena@broadinstitute.org>
2022-06-25 20:05:45 -0400

    WIP - subtype/reference/segment dynamic DAG

--- workflow_main/scripts/write_reference_files.py
+++ workflow_main/scripts/write_reference_files.py
@@ -9,6 +9,7 @@ Author: Albert Chen - Vector Engineering Team (chena@broadinstitute.org)
 import argparse
 import json
 import pandas as pd
+from pathlib import Path
 
 from fasta import read_fasta_file
 
@@ -17,7 +18,13 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--reference-fasta", type=str, required=True, help="Reference FASTA file"
+        "--reference-path", type=str, required=True, help="Reference FASTA file"
+    )
+    parser.add_argument(
+        "--subtypes", type=str, nargs="+", required=True, help="All subtypes"
+    )
+    parser.add_argument(
+        "--segments", type=str, nargs="+", required=True, help="All segments"
     )
     parser.add_argument(
         "--primers-csv", type=str, required=True, help="Primers CSV file"
@@ -30,14 +37,31 @@ def main():
     )
     args = parser.parse_args()
 
-    # Write the reference fasta file to json
-    # Load the reference sequence
-    with open(args.reference_fasta, "r") as fp:
-        lines = fp.readlines()
-        ref_seqs = read_fasta_file(lines)
+    ref_obj = {}
+    for subtype in args.subtypes:
+        ref_obj[subtype] = {}
+
+        # Find individual references within each subtype
+        reference_paths = [
+            d
+            for d in sorted((Path(args.reference_path) / subtype).iterdir())
+            if d.is_dir()
+        ]
+        for reference_path in reference_paths:
+            reference = reference_path.name
+            ref_obj[subtype][reference] = {}
+            ref_obj[subtype][reference]["name"] = reference
+            ref_obj[subtype][reference]["segments"] = {}
+            for segment in args.segments:
+                with (reference_path / f"{segment}.fa").open("r") as fp:
+                    lines = fp.readlines()
+                    records = read_fasta_file(lines)
+                    ref_obj[subtype][reference]["segments"][segment] = list(
+                        records.values()
+                    )[0]["sequence"]
 
     with open(args.reference_json, "w") as fp:
-        fp.write(json.dumps(ref_seqs))
+        fp.write(json.dumps(ref_obj))
 
     # Load primers, write to JSON
     primers_df = pd.read_csv(args.primers_csv, comment="#")
