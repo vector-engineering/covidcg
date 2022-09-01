db38194529868e5fff94b6c1c656be1195ed0cc7
Albert Tian Chen <chena@broadinstitute.org>
2022-09-01 00:54:20 -0400

    For isolates with subtype in the isolate/virus name but no subtype assignment from minimap2, default to the isolate name (where it is obvious). Saves some sequences

--- workflow_rsv_genbank_ingest/scripts/join_subtype_assignments.py
+++ workflow_rsv_genbank_ingest/scripts/join_subtype_assignments.py
@@ -20,6 +20,12 @@ def main():
     parser.add_argument(
         "--out-metadata", type=str, required=True, help="Output metadata CSV file"
     )
+    parser.add_argument(
+        "--out-failed",
+        type=str,
+        required=True,
+        help="Output metadata CSV file with only records that did not get assigned a subtype. For debugging purposes only",
+    )
 
     args = parser.parse_args()
 
@@ -52,10 +58,57 @@ def main():
     metadata = pd.read_csv(args.metadata, index_col="Accession ID")
 
     # Join assignments onto virus DF
-    metadata = metadata.join(serotype_assignments, how="inner")
+    metadata = metadata.join(serotype_assignments, how="left")
 
     # metadata.loc[:, 'serotype'] = metadata['serotype'].combine_first(metadata['assign_serotype'])
     # metadata.drop(columns=['assign_genus', 'assign_serotype'], inplace=True)
+
+    # Special cases for sequencing of non-G genes but subtype is in the virus/isolate name
+    # Only making these exceptions for where the name is very clearly deliniating A vs. B
+    # TODO: in the GenBank entries for some of these viruses that are don't have A/B in the name,
+    #       the GenBank entry has, in the "source" field under "FEATURES", clearly marked "genotype"
+    #       or "subgroup" fields. Maybe we can mine and extract these from the NCBI virus API?
+    subtype_A_isolate_matches = [
+        "RSVA",
+        r"^A89",
+        r"^A9[0-9]-[0-9]{2,3}-[0-9]{2}",
+        # C0607-1015-A
+        r"^[CH][0-9]{4}-[0-9]{3,4}-A",
+        # RSV/177-A/Nairobi/2008
+        r"^RSV\/[0-9]{2,3}-A",
+        r"RSVs?\/Tehran\.IRN\/A\/",
+        # Ghom/A/12/1363
+        r"Ghom\/A\/",
+    ]
+    subtype_B_isolate_matches = [
+        "RSVB",
+        r"^B89",
+        r"^B9[0-9]-[0-9]{2,3}-[0-9]{2}",
+        # B941083-05
+        r"^B94[0-9]{4}-[0-9]{2}",
+        r"^[CH][0-9]{4}-[0-9]{3,4}-B",
+    ]
+
+    for subtype_A_isolate_match in subtype_A_isolate_matches:
+        A_match = (
+            metadata["virus_name"].str.contains(subtype_A_isolate_match)
+            & metadata["subtype"].isna()
+        )
+        metadata.loc[A_match, "subtype"] = "A"
+        metadata.loc[A_match, "genotype"] = "Unknown"
+
+    for subtype_B_isolate_match in subtype_B_isolate_matches:
+        B_match = (
+            metadata["virus_name"].str.contains(subtype_B_isolate_match)
+            & metadata["subtype"].isna()
+        )
+        metadata.loc[B_match, "subtype"] = "B"
+        metadata.loc[B_match, "genotype"] = "Unknown"
+
+    # Save failed subtype assignment to separate file for debugging
+    metadata.loc[metadata["subtype"].isna() | metadata["genotype"].isna()].to_csv(
+        args.out_failed
+    )
 
     # Remove viruses without serotype assignment
     metadata.drop(metadata.index[metadata["subtype"].isna()], inplace=True)
