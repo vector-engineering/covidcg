f1ce385a8edaed628f088ffd7c6386f9c5e1e81e
Albert Tian Chen <chena@broadinstitute.org>
2023-10-30 21:20:42 -0400

    Add exclusion reasons

--- workflow_main/scripts/preprocess_sars2_sequences.py
+++ workflow_main/scripts/preprocess_sars2_sequences.py
@@ -18,7 +18,7 @@ def main():
     """Filter out sequences (adapted from van Dorp et al, 2020)
     1. Filter against nextstrain exclusion list
     2. Can't be less than 29700NT
-	3. Can't have more than 5% ambiguous NT
+    3. Can't have more than 5% ambiguous NT
     """
 
     parser = argparse.ArgumentParser()
@@ -30,8 +30,12 @@ def main():
         required=True,
         help="Path to nextstrain exclusion file",
     )
+    parser.add_argument("--exclude-list", type=str, required=True, help="Debug")
     parser.add_argument(
-        "--output", type=str, required=True, help="Output FASTA file",
+        "--output",
+        type=str,
+        required=True,
+        help="Output FASTA file",
     )
 
     args = parser.parse_args()
@@ -49,6 +53,7 @@ def main():
     num_excluded = 0
     fp_in = gzip.open(args.input, "rt")
     fp_out = gzip.open(args.output, "wt")
+    fp_exclude = open(args.exclude_list, "a")
 
     cur_entry = ""
     cur_seq = ""
@@ -57,24 +62,30 @@ def main():
 
         # Beginning of a new entry, or EOF = end of current entry
         if not line or line[0] == ">":
-
             if cur_entry:
                 num_ambiguous = 0
                 for char in cur_seq:
                     if char == "N":
                         num_ambiguous += 1
 
-                if (
-                    # 1: Check against nextstrain exclusion list
-                    (cur_entry in exclude_taxons)
-                    or
-                    # 2: Can't be less than 29700 NT
-                    len(cur_seq) < 29700
-                    or
-                    # 3: Can't have more than 5% ambiguous (N) NT
-                    num_ambiguous > math.floor(len(cur_seq) * 0.05)
-                ):
+                exclude_reasons = []
+                # 1: Check against nextstrain exclusion list
+                if cur_entry in exclude_taxons:
                     num_excluded += 1
+                    exclude_reasons.append("in_exclusion_list")
+
+                # 2: Can't be less than 29700 NT
+                if len(cur_seq) < 29700:
+                    num_excluded += 1
+                    exclude_reasons.append(f"too_short:{str(len(cur_seq))}")
+
+                # 3: Can't have more than 5% ambiguous (N) NT
+                if num_ambiguous > math.floor(len(cur_seq) * 0.05):
+                    num_excluded += 1
+                    exclude_reasons.append(f"too_many_ambiguous:{str(num_ambiguous)}")
+
+                if len(exclude_reasons) > 1:
+                    fp_exclude.write(f"{cur_entry},{';'.join(exclude_reasons)}\n")
                 else:
                     # It passed, write to output
                     fp_out.write(">" + cur_entry + "\n")
@@ -101,6 +112,7 @@ def main():
 
     fp_in.close()
     fp_out.close()
+    fp_exclude.close()
 
     print("Removed {:,} sequences".format(num_excluded), flush=True)
 
