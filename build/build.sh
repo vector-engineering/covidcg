8411d194675aae618d868a9b3e4439595509ae56
Albert Tian Chen <chena@broadinstitute.org>
2025-07-16 00:08:55 -0400

    Flu genbank - sanitize segment metadata before integer casting (#656)
    * consolidate build commands
    
    * sanitize segment field before int casting to avoid crashes during metadata processing

--- build/build.sh
+++ build/build.sh
@@ -10,18 +10,10 @@ else
     echo "Building version: ${CG_VERSION}"
 fi
 
-# SARS2
-
 gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg",_CONFIGFILE="config/config_sars2_gisaid.yaml",_TAG_NAME="${CG_VERSION}", . && \
 gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg-genbank",_CONFIGFILE="config/config_sars2_genbank.yaml",_TAG_NAME="${CG_VERSION}" . && \
 gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg-private",_CONFIGFILE="config/config_sars2_gisaid_private.yaml",_TAG_NAME="${CG_VERSION}" . && \
-gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg-alpha",_CONFIGFILE="config/config_sars2_alpha.yaml",_TAG_NAME="${CG_VERSION}", .
-
-# RSV
-
-gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="rsv",_CONFIGFILE="config/config_rsv_genbank.yaml",_TAG_NAME="${CG_VERSION}" .
-
-# FLU
-
-gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="flu-gisaid",_CONFIGFILE="config/config_flu_gisaid.yaml",_TAG_NAME="${CG_VERSION}" .
+gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="cg-alpha",_CONFIGFILE="config/config_sars2_alpha.yaml",_TAG_NAME="${CG_VERSION}", . && \
+gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="rsv",_CONFIGFILE="config/config_rsv_genbank.yaml",_TAG_NAME="${CG_VERSION}" . && \
+gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="flu-gisaid",_CONFIGFILE="config/config_flu_gisaid.yaml",_TAG_NAME="${CG_VERSION}" . && \
 gcloud builds submit --config build/cloudbuild.yaml --substitutions=_TARGET="flu-genbank",_CONFIGFILE="config/config_flu_genbank.yaml",_TAG_NAME="${CG_VERSION}" .
